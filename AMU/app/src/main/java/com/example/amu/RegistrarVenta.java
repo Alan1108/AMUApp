7bbcda71cf0b0028248c519ddb4f945f916af182
elianjl <ejllorente@espe.edu.ec>
2021-07-29 01:05:04 -0500

    Interfaces finales

---
+++ AMU/app/src/main/java/com/example/amu/RegistrarVenta.java
@@ -0,0 +1,14 @@
+package com.example.amu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrarVenta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_venta);
+    }
+}
